aecd5fda7bbdc9bfa32d8cd3b91a753db1584a98
Kimo <mk5391130@gmail.com>
2024-07-29 14:21:29 +0300

    Project_Structure

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template lang="pug">
+header 
+ div.wrapper 
+   h1 Starter of The Project !
+
+</template>
+
+<style  lang="scss">
+
+@mixin perfect-center{
+ display:flex;
+ justify-content:center;
+ align-items:center;
+}
+
+header {
+  width:100%;
+  line-height: 1.5;
+  max-height: 100vh;
+  .wrapper{
+    @include perfect-center;
+
+  }
+
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    // empty for now
+  ]
+})
+
+export default router
